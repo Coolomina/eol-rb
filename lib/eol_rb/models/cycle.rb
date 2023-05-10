f7091ed2e0d891277ca83b7333ff81e13deadba5
Kat Tipton <kat.tipton@ext.chime.com>
2023-05-10 14:07:04 -0400

    Support boolean eol value

--- lib/eol_rb/models/cycle.rb
+++ lib/eol_rb/models/cycle.rb
@@ -8,7 +8,7 @@ module EOL
 
       def initialize(specs)
         @version = Gem::Version.new(specs.fetch("cycle"))
-        @eol = Date.parse(specs.fetch("eol"))
+        @eol = parse_eol(specs.fetch("eol"))
         @latest = Gem::Version.new(specs.fetch("latest"))
         @latest_release_date = Date.parse(specs.fetch("latestReleaseDate"))
         @release_date = Date.parse(specs.fetch("releaseDate"))
@@ -16,7 +16,19 @@ module EOL
       end
 
       def supported?
+        return !eol if [true, false].include? eol
+
         @eol > Time.now.to_date
+      end
+
+      private
+
+      def parse_eol(eol)
+        return false if eol.nil?
+        return eol if [true, false].include? eol
+        return Date.parse(eol) if eol.is_a? String
+
+        nil
       end
     end
   end
